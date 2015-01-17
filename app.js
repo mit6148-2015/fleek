f471e970a140ef3df3414138800167190d7e4d89
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-16 23:00:31 -0500

    initial website

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile('/public/views/index.html', {root: __dirname })
+});
+
+app.listen(app.get('port'), function() {
+    console.log("browsing on fleek at localhost:" + fleek.get('port'));
+});
