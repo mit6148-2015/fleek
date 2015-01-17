e6e8769009149665ac83e7d06d4bf4a73a4e7de2
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-17 02:06:23 -0500

    router now working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.sendFile('/public/views/index.html', {root: __dirname });
+    res.sendFile(path.join(__dirname,'../../public/views/index.html'));
 });
 
 module.exports = router;
