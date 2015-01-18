03ee34054ab8ebec5d1c52503ef4a3ca1fd5b1ba
kimberli <kimberlizhong@gmail.com>
2015-01-18 14:21:32 -0500

    clean up controller code

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -1,50 +1,60 @@
 (function() {
+	//make app
 	var app = angular.module('fleekApp', []);
 
+	//page controller - controls what the page displays & authentication state (need to fix the auth stuff)
 	app.controller("pageController", function ($scope, $http, $location) {
+		//default page is splash
 		$scope.current= "splash";
+		//default authentication state is false
 		$scope.isAuthenticated = false;
+		//function to set view (accessible from child scopes)
 		$scope.setView = function(page) {
 			$scope.current = page;
 		};
+		//function to set authentication (accessible from child scopes) - NEEDS TO BE DONE DIFFERENTLY
 		$scope.setAuthentication = function(auth) {
 			$scope.isAuthenticated = auth;
 		}
+		//function to log out - NEEDS TO BE DONE DIFFERENTLY
 		$scope.logout = function() {
 			$http.get('../../logout')
 	        .success(function(data) {
 	            $scope.isAuthenticated=false;
 	       		$scope.setView('splash');
-	            console.log('logout successful');
+	            console.log(data);
 	        })
 		}
 	});
 
+	//login controller - controls login form
 	app.controller("loginController", function($scope,$http) {
+		//default values
 		$scope.submitted = false;
-		$scope.success = false;
 		$scope.err = false;
+		//on form submit, send POST to /login with data
 	    $scope.submit = function(){
 	    	$scope.submitted = true;
 	    	$http.post('../../login', {username: angular.lowercase($scope.user), password: $scope.pass})
 		    	.success(function(data) {
-		    		$scope.success = true;
-		    		console.log('success ' + angular.lowercase($scope.user));
 		    		$scope.setView('problem');
 		    		$scope.setAuthentication(true);
+		    		console.log(data);
 		    	})
 		    	.error(function(data) {
 		    		$scope.err = true;
-		    		console.log('fail');
 		    		$scope.submitted = false;
+		    		console.log(data);
 		    	})
 	    }
 	});
 
+	//signup controller - controls signup form
 	app.controller("signupController", function($scope,$http) {
+		//default values
 		$scope.submitted = false;
-		$scope.success = false;
 		$scope.err = false;
+		//get country list for form
 		$scope.countries = null;
 		$http.get('../assets/countries.json')
 	        .success(function(data) {
@@ -52,24 +62,26 @@
 	        })
 	        .error(function(data) {
 	        	$scope.countries=[{"name": "USA"}];
+	        	console.log(data);
 	        });
+	    //on form submit, send POST to /signup with data
 	    $scope.submit = function(){
 	    	$scope.submitted = true;
 	    	$http.post('../../signup', {username: angular.lowercase($scope.user), password: $scope.pass, gender: $scope.gen, country: $scope.ctry})
 		    	.success(function(data) {
-		    		$scope.success = true;
-		    		console.log('success ' + angular.lowercase($scope.user));
 		    		$scope.setView('problem');
 		    		$scope.setAuthentication(true);
+		    		console.log(data);
 		    	})
 		    	.error(function(data) {
 		    		$scope.err = true;
-		    		console.log('fail');
 		    		$scope.submitted = false;
+		    		console.log(data);
 		    	})
 	    }
 	});
 
+	//problem view controller - add stuff here later
 	app.controller("problemController", function($scope,$http) {
 	});
 
@@ -109,6 +121,7 @@
 	app.directive("problem", function() {
 		return {
 			restrict: 'E',
+			url: 'browse',
 			templateUrl: '../views/problem.html'
 		};
 	});
