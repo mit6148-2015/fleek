b585b50fdcee01c326b2c0104851c009fe038288
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-18 03:44:59 -0500

    sets up Passport

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,21 @@
 var express = require('express');
-var path = require('path');
 var router = express.Router();
+var path = require('path');
+var passport = require('passport');
 
+// landing page
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'../../public/views/index.html'));
 });
 
+// login via Passport
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/', 
+                                     failureRedirect: '/login', 
+                                     failureFlash: true })
+);
+
+// provides list of countries
 router.get('/public/assets/countries.json', function(req, res) {
     res.sendFile(path.join(__dirname,'../../public/assets/countries.json'));
 });
