1d16d3605f3b7ec1aae2694c0e447e413c17e72f
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-21 07:45:57 -0500

    add AIME answers

---
+++ scraping/addToDatabase/aime.js
@@ -0,0 +1,29 @@
+// setup database
+var mongoose = require('mongoose');
+mongoose.connect('PATH_TO_MONGO');
+
+// load object constructors
+var problemMeta = require('../../server/models/prototypes/problemMeta');
+var response = require('../../server/models/prototypes/response');
+
+// load models
+var Problem = require('../../server/models/problem');
+
+// load data from JSON file
+var fs = require('fs');
+var data = JSON.parse(fs.readFileSync('../scrapers/aime.json', 'utf8'));
+
+for (var i=0; i<data.length; i++) {
+    var curdatum = data[i];
+
+    Problem.create({
+        meta: new problemMeta.aime(curdatum.year, curdatum.name_modifier, curdatum.problem_number - 1),
+        source: {
+            name: curdatum.source_name,
+            url: curdatum.source_link,
+        },
+        statement: curdatum.problem_statement,
+        response: new response.integerAnswer(0)
+    });
+
+}
