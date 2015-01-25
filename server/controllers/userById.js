7e0f2540223ee2e7e84a7fcb490847686213500b
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-24 23:31:32 -0500

    routing clean up and db functionality

---
+++ server/controllers/userById.js
@@ -0,0 +1,13 @@
+var ObjectId = require('mongoose').Types.ObjectId; 
+var User = require('../models/user');
+
+function userById (req, res) {
+    User.findOne( { _id : ObjectId(req.query.id) },function (err, user) {
+        if (err)
+            console.log(err);
+
+        res.send(user);
+    });
+};
+
+module.exports = userById;
