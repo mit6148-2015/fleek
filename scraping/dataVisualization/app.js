3644b98aa9cd35ee43bbbaaef14ec11e575d7b06
akshayravikumar <akshayr@mit.edu>
2015-01-25 08:59:07 -0800

    trying data stuff

---
+++ scraping/dataVisualization/app.js
@@ -0,0 +1,13 @@
+//var app = require('express')();
+var express = require('express');
+var app = express();
+console.log("stating fleek");
+var server = require('http').Server(app);
+var path = require('path');
+
+server.listen(3000);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/charttest.html');
+});
+
