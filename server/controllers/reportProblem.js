0265d9819d4a0f9d63c1ed39cd0adb4208ded9bb
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-25 02:17:25 -0500

    trying to fix reporting

--- server/controllers/reportProblem.js
+++ server/controllers/reportProblem.js
@@ -1,21 +1,13 @@
 var fs = require('fs');
-var pathToOutput = '../../public/assets/reportedProblems.txt'
 
 function reportProblem(req, res) {
-    var problemId = String(req.body.id);
-    fs.open(pathToOutput, 'a', function(err, fid) {
-        if (err)
+    fs.appendFile("../../public/assets/reportedProblems.txt", req.body.id, function(err) {
+        if(err) {
             console.log(err);
-
-        fs.appendFile(fid, problemId, function(err) {
-            if (err)
-                console.log(err);
-
-            res.send(problemId);
-
-            fs.close(fid);
-        })
-    });
+        } else {
+            res.send(req.body.id);
+        }
+    }); 
 }
 
 module.exports = reportProblem;
