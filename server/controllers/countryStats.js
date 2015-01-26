99d42ced52c3d040291c9f383acb45c30f4273c3
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-26 10:26:59 -0500

    fixed empty users bug

--- server/controllers/countryStats.js
+++ server/controllers/countryStats.js
@@ -6,9 +6,10 @@ function countryStats(req, res) {
         if (err)
             console.log(err);
 
-        if (users.length > 0) 
+        if (users.length > 0) {
             var stats = {};
             doForIndex(0);
+        }
 
         function doForIndex(index) {
             var country = users[index].country;
