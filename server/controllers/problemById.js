d4251b26a9ca80d79728c094cc6bdfe590c63270
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-27 02:04:24 -0500

    populate tags when serving problem

--- server/controllers/problemById.js
+++ server/controllers/problemById.js
@@ -2,7 +2,7 @@ var ObjectId = require('mongoose').Types.ObjectId;
 var Problem = require('../models/problem');
 
 function problemById (req, res) {
-    Problem.findOne( { _id : ObjectId(req.query.id) },function (err, problem) {
+    Problem.findOne( { _id : ObjectId(req.query.id) }).populate('tags').exec(function (err, problem) {
         if (err)
             console.log(err);
 
