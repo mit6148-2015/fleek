402adc75c03f465e53777fd8eab87c1d2928c6d6
kimberli <kimberlizhong@gmail.com>
2015-01-27 07:16:54 -0500

    add search page tutorial

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -1,8 +1,9 @@
 //search view controller - add stuff here later
 angular.module('fleekApp').controller("searchController", function($scope,$rootScope,$q,$routeParams,$location,DataService) {
-	$scope.searchQuery = $routeParams.searchQuery;
-	$rootScope.searchHistory = $scope.searchQuery;
-	$scope.predicate = 'meta.setPattern';
+	$scope.searchQuery = $routeParams.searchQuery; //set search query to URL parameter
+	$rootScope.searchHistory = $scope.searchQuery; //set search history to last recorded search
+	$scope.predicate = 'meta.setPattern'; //key to sort table by
+	$scope.tutorial; //show helpbox
 	//data for form validation
 	$scope.tagList = [];
 	$scope.minYear = 1950;
@@ -13,6 +14,11 @@ angular.module('fleekApp').controller("searchController", function($scope,$rootS
 	$scope.endYear = 2015;
 	//holds search results
 	$scope.results;
+	//see if user should see tutorial
+	DataService.getData('/tutorial/status')
+	.then (function(data) {
+		$scope.tutorial = data.tutorial.search;
+	})
 	//generate list of contests from a GET request
 	DataService.getData('/db/list/sets')
 	.then (function(data) {
@@ -34,6 +40,13 @@ angular.module('fleekApp').controller("searchController", function($scope,$rootS
 	.then (function(data) {
 		$scope.tagList = data;
 	});
+	//create links for search results
+	$scope.closeTutorial = function() {
+		DataService.setTutorial('search',false)
+		.then (function(data) {
+			$scope.tutorial = false;
+		})
+	}
 	//return promise with tag list for autocomplete
 	$scope.loadItems = function(query) {
 		matchingTags = [];
--- public/services/DataService.js
+++ public/services/DataService.js
@@ -1,80 +1,95 @@
 angular.module('fleekApp').factory('DataService', function($http, $q){
 	return {
-	        getData: function(path) {
-	            return $http.get(path)
-	                .then(function(response) {
-	                    if (typeof response.data === 'object') {
-	                    	console.log("\tgetData response from " + path + ": " + typeof response.data);
-	                        return response.data;
-	                    } 
-	                    else {
-	                        return $q.reject(response.data);
-	                    }
+        getData: function(path) {
+            return $http.get(path)
+                .then(function(response) {
+                    if (typeof response.data === 'object') {
+                    	console.log("\tgetData response from " + path + ": " + typeof response.data);
+                        return response.data;
+                    } 
+                    else {
+                        return $q.reject(response.data);
+                    }
 
-	                }, function(response) {
-	                    return $q.reject(response.data);
-	            	}
-	            );
-	        },
-	        search: function(text,tags,conts,syear,eyear) {
-	        	return $http.get('/db/query/problems', { params: {q: text, tags: tags, setPatterns: conts, startYear: syear, endYear: eyear}})
-	        		.then(function(response) {
-	        			if (typeof response.data === 'object') {
-							console.log("\tSearch response " + response.data.length);
-	        				return response.data;
-	        			}
-	        			else {
-	        				return $q.reject(response.data);
-	        			}
-	        		}, function(response) {
-	        			return $q.reject(response.data);
-	        		}
-	        	);
-	        },
-	        getProblem: function(problemId) {
-	        	return $http.get('/db/problem', { params: {id: problemId}})
-	        		.then(function(response) {
-	        			if (typeof response.data === 'object') {
-							console.log("\tProblem response " + response.data._id);
-	        				return response.data;
-	        			}
-	        			else {
-	        				return $q.reject(response.data);
-	        			}
-	        		}, function(response) {
-	        			return $q.reject(response.data);
-	        		}
-	        	);
-	        },
-	        getSet: function(setId) {
-	        	return $http.get('/db/setproblems', { params: {id: setId}})
-	        		.then(function(response) {
-	        			if (typeof response.data === 'object') {
-							console.log("\tSet response " + response.data._id);
-	        				return response.data;
-	        			}
-	        			else {
-	        				return $q.reject(response.data);
-	        			}
-	        		}, function(response) {
-	        			return $q.reject(response.data);
-	        		}
-	        	);
-	        },
-	        sendProblemResult : function(path,problemId) {
-	        	return $http.post(path, { params: {id: problemId}})
-	        		.then(function(response) {
-	        			if (typeof response.data === 'string') {
-	        				console.log(response.data);
-	        				return response.data;
-	        			}
-	        			else {
-	        				return $q.reject(response.data);
-	        			}
-	        		}, function(response) {
-	        			return $q.reject(response.data);
-	        		}
-	        	);
-	        },
-	    };
+                }, function(response) {
+                    return $q.reject(response.data);
+            	}
+            );
+        },
+        search: function(text,tags,conts,syear,eyear) {
+        	return $http.get('/db/query/problems', { params: {q: text, tags: tags, setPatterns: conts, startYear: syear, endYear: eyear}})
+        		.then(function(response) {
+        			if (typeof response.data === 'object') {
+						console.log("\tSearch response " + response.data.length);
+        				return response.data;
+        			}
+        			else {
+        				return $q.reject(response.data);
+        			}
+        		}, function(response) {
+        			return $q.reject(response.data);
+        		}
+        	);
+        },
+        getProblem: function(problemId) {
+        	return $http.get('/db/problem', { params: {id: problemId}})
+        		.then(function(response) {
+        			if (typeof response.data === 'object') {
+						console.log("\tProblem response " + response.data._id);
+        				return response.data;
+        			}
+        			else {
+        				return $q.reject(response.data);
+        			}
+        		}, function(response) {
+        			return $q.reject(response.data);
+        		}
+        	);
+        },
+        getSet: function(setId) {
+        	return $http.get('/db/setproblems', { params: {id: setId}})
+        		.then(function(response) {
+        			if (typeof response.data === 'object') {
+						console.log("\tSet response " + response.data._id);
+        				return response.data;
+        			}
+        			else {
+        				return $q.reject(response.data);
+        			}
+        		}, function(response) {
+        			return $q.reject(response.data);
+        		}
+        	);
+        },
+        sendProblemResult : function(path,problemId) {
+        	return $http.post(path, { params: {id: problemId}})
+        		.then(function(response) {
+        			if (typeof response.data === 'string') {
+        				console.log(response.data);
+        				return response.data;
+        			}
+        			else {
+        				return $q.reject(response.data);
+        			}
+        		}, function(response) {
+        			return $q.reject(response.data);
+        		}
+        	);
+        },
+        setTutorial : function(name,state) {
+        	return $http.post('/tutorial/'+name, { params: {name: state}})
+        		.then(function(response) {
+        			if (typeof response.data === 'string') {
+        				console.log(response.data);
+        				return response.data;
+        			}
+        			else {
+        				return $q.reject(response.data);
+        			}
+        		}, function(response) {
+        			return $q.reject(response.data);
+        		}
+        	);
+        },
+    };
 });
