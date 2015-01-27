5c27099dfcc00e89d75595ac2a8f8433da1fb099
Vahid Fazel-Rezai <vahid_fazel@yahoo.com>
2015-01-27 08:15:21 -0500

    fix update tutorial

--- server/controllers/updateShort.js
+++ server/controllers/updateShort.js
@@ -1,7 +1,19 @@
 var User = require('../models/user');
 
 function updateShort(req, res) {
-    User.findOneAndUpdate({'username': req.user.username}, {$set: { 'tutorial': {'short': req.body.params.state}}});
+
+    User.findById(req.user.id, function (err, user) {
+        if (err) 
+            console.log(err)
+
+        if (user) {
+            user.tutorial.short = req.body.params.state;
+            user.markModified('tutorial');
+            user.save();
+        } else {
+            console.log('User not found');
+        }
+    });
     res.send('Updated tutorial.short');
 }
 
