43eaada4605272987f9c8b854a93028c189bea7d
kimberli <kimberlizhong@gmail.com>
2015-01-27 10:26:31 -0500

    hint reset fix

--- public/controllers/profileController.js
+++ public/controllers/profileController.js
@@ -29,16 +29,8 @@ angular.module('fleekApp').controller("profileController", function($scope,$rout
 		})
 	});
 
-	//see if user should see tutorial
-    DataService.getData('/tutorial/status')
-    .then (function(data) {
-        $scope.tutorial = data.tutorial;
-    })
-
 	$scope.resetTutorial = function() {
-		for (key in $scope.tutorial) {
-			DataService.setTutorial(key,true);
-		}
+		DataService.getData('/tutorial/reset');
 		$scope.reset = true;
 	}
 });
