23281b9bd943eccf4f55a269dd48e15dd064595b
kimberli <kimberlizhong@gmail.com>
2015-01-27 10:06:59 -0500

    better profile loading

--- public/controllers/profileController.js
+++ public/controllers/profileController.js
@@ -2,6 +2,7 @@
 angular.module('fleekApp').controller("profileController", function($scope,$routeParams,$timeout,DataService) {
 	$scope.username = $routeParams.username; //email address
 	$scope.user; //user object
+	$scope.reset = false;
 
 	//get user data
 	DataService.getUser($scope.username)
@@ -11,8 +12,23 @@ angular.module('fleekApp').controller("profileController", function($scope,$rout
 	.then (function() {
 		$timeout(function() {
 			loadchart($scope.user.stats.solvedCount,$scope.user.stats.attemptedCount - $scope.user.stats.solvedCount,'user-graph');
-		},750);
+		},250);
 	});
+
+	//hacky jquery to help make sure chart loads
+	$(window).load(function() {
+		//get user data
+		DataService.getUser($scope.username)
+		.then(function(data) {
+			$scope.user = data;
+		})
+		.then(function() {
+			$timeout(function() {
+				loadchart($scope.user.stats.solvedCount,$scope.user.stats.attemptedCount - $scope.user.stats.solvedCount,'user-graph');
+			},250);
+		})
+	});
+
 	//see if user should see tutorial
     DataService.getData('/tutorial/status')
     .then (function(data) {
@@ -23,5 +39,6 @@ angular.module('fleekApp').controller("profileController", function($scope,$rout
 		for (key in $scope.tutorial) {
 			DataService.setTutorial(key,true);
 		}
+		$scope.reset = true;
 	}
 });
